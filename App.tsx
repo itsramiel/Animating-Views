71974f02727b7f7952259f5d3ce0d5409f3c5c45
Rami Elwan <ramielwan48@gmail.com>
2024-07-16 20:02:46 +0200

    feat: make main layout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.container}>
+        <View style={styles.button}>
+          <Text style={styles.text}>Left</Text>
+        </View>
+        <View style={styles.button}>
+          <Text style={styles.text}>Right</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  container: {
+    flexDirection: "row",
+    paddingHorizontal: 16,
+    gap: 10,
+  },
+  button: {
+    backgroundColor: "red",
+    flex: 1,
+    alignItems: "center",
+    padding: 16,
+    borderRadius: 30,
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
   },
 });
