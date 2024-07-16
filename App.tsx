4a1c74d2fd550cf45497d615e0bf52df704a6c56
Rami Elwan <ramielwan48@gmail.com>
2024-07-16 20:13:38 +0200

    feat: reproduce issue

--- App.tsx
+++ App.tsx
@@ -1,6 +1,37 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import Animated, {
+  EntryAnimationsValues,
+  ExitAnimationsValues,
+  LayoutAnimation,
+  LinearTransition,
+  withTiming,
+} from "react-native-reanimated";
+
+function ExitAnimation(values: ExitAnimationsValues): LayoutAnimation {
+  "worklet";
+  return {
+    initialValues: {
+      width: values.currentWidth,
+    },
+    animations: {
+      width: withTiming(0, { duration: 5000 }),
+    },
+  };
+}
+
+function EnterAnimation(values: EntryAnimationsValues): LayoutAnimation {
+  "worklet";
+  return {
+    initialValues: {
+      width: 0,
+    },
+    animations: {
+      width: withTiming(values.targetWidth, { duration: 5000 }),
+    },
+  };
+}
 
 export default function App() {
   const [isOnlyRight, setIsOnlyRight] = useState(true);
@@ -9,13 +40,20 @@ export default function App() {
     <View style={styles.screen}>
       <View style={styles.container}>
         {isOnlyRight ? null : (
-          <View style={styles.button}>
+          <Animated.View
+            exiting={ExitAnimation}
+            entering={EnterAnimation}
+            style={styles.button}
+          >
             <Text style={styles.text}>Left</Text>
-          </View>
+          </Animated.View>
         )}
-        <View style={styles.button}>
+        <Animated.View
+          style={styles.button}
+          layout={LinearTransition.duration(5000)}
+        >
           <Text style={styles.text}>Right</Text>
-        </View>
+        </Animated.View>
       </View>
       <Button title="Toggle" onPress={() => setIsOnlyRight(!isOnlyRight)} />
       <StatusBar style="auto" />
